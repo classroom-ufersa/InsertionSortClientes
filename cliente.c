a15e8d31507691c949751e9c3acbbbe7cf93b40c
DanielNeres <danielnerres@gmail.com>
2024-02-06 17:20:46 -0300

    Criação da struct clientes

--- cliente.c
+++ cliente.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "cliente.h"
 
+struct clientes{
+    char Nome[80];
+    char Endereco[80];
+    int Codigo_de_Cliente;
+}
+
+
 Ler_dados(){
 
     fopen("Clientes.txt", "rt");

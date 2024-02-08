3e8104f9bb06b0350185a9e823265fc8c00fed68
Isabelly Lima <mslima5643@gmail.com>
2024-02-08 13:57:22 -0300

    Adição do for

--- cliente.c
+++ cliente.c
@@ -10,16 +10,22 @@ struct clientes{
 
 void Escrever_dados(){
 
-    int Quantidade_Clientes;
+    int Quantidade_Clientes; int cont;
     printf("Digite a Quantidade de Clientes:\t");
     scanf("%d", &Quantidade_Clientes);
     struct clientes *Clientes_Var = (struct clientes *)malloc(sizeof(struct clientes) * Quantidade_Clientes);
     FILE *Arquivo_Clientes = fopen("Clientes.txt", "wr");
     fclose(Arquivo_Clientes);
-    printf("Digite o nome do cliente:\n");
+
+    for (cont=0; cont<Quantidade_Clientes;cont++)
+    {
+        printf("Digite o nome do cliente:\n");
     scanf("%s",Clientes_Var->Nome);
     printf("Digite o endereco do cliente:\n");
     scanf("%s", Clientes_Var->Endereco);
     printf("Digite o codigo do cliente:\n")
     printf("%s", Clientes_Var->Codigo_de_Cliente);
+    }
+    
+    
 }

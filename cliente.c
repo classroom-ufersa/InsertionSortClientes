4e6d8ce0b0250c9268a22f144df3486dff1d2c77
DanielNeres <danielnerres@gmail.com>
2024-02-10 10:28:44 -0300

    Modificação da Função Escrever_dados

--- cliente.c
+++ cliente.c
@@ -14,26 +14,33 @@ void Escrever_dados(){
     printf("Digite a Quantidade de Clientes:\t");
     scanf("%d", &Quantidade_Clientes);
     Clientes *Clientes_Var = (Clientes *)malloc(sizeof(Clientes) * Quantidade_Clientes);
-    FILE *Arquivo_Clientes = fopen("Clientes.txt", "wr");
 
     for (contador = 0; contador < Quantidade_Clientes; contador++)
     {
-        fprintf(Arquivo_Clientes, "Dados dos clientes:\n");
 
         printf("Digite o nome do cliente:\n");
         scanf(" %[^\n]", Clientes_Var[contador].Nome);
-        fprintf(Arquivo_Clientes, "Nome:  %s\n", Clientes_Var[contador].Nome);
 
         printf("Digite o endereco do cliente:\n");
         scanf(" %[^\n]", Clientes_Var[contador].Endereco);
-        fprintf(Arquivo_Clientes, "Endereco:  %s\n", Clientes_Var[contador].Endereco);
 
         printf("Digite o codigo do cliente:\n");
         scanf("%d", &Clientes_Var[contador].Codigo_de_Cliente);
+
+        void insertionSort(Clientes_Var, contador);
+    }
+
+    FILE *Arquivo_Clientes = fopen("Clientes.txt", "wr");
+    for (contador = 0; contador < Quantidade_Clientes; contador++)
+    {
+        
+        fprintf(Arquivo_Clientes, "Dados dos clientes %d:\n", contador + 1);
+        fprintf(Arquivo_Clientes, "Nome:  %s\n", Clientes_Var[contador].Nome);
+        fprintf(Arquivo_Clientes, "Endereco:  %s\n", Clientes_Var[contador].Endereco);
         fprintf(Arquivo_Clientes, "Codigo de Cliente:  %d\n", Clientes_Var[contador].Codigo_de_Cliente);
     }
-    
     fclose(Arquivo_Clientes);
+
 }
 
 void insertionSort(Clientes *Clientes_Var, int Quantidade_Clientes)

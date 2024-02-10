7a77672d8ccd528e8a1ff1dde8ea3a2840544315
DanielNeres <danielnerres@gmail.com>
2024-02-10 09:17:03 -0300

    Modificação da Função Escrever_dados

--- cliente.c
+++ cliente.c
@@ -10,22 +10,29 @@ struct clientes{
 
 void Escrever_dados(){
 
-    int Quantidade_Clientes; int cont;
+    int Quantidade_Clientes; int contador;
     printf("Digite a Quantidade de Clientes:\t");
     scanf("%d", &Quantidade_Clientes);
     struct clientes *Clientes_Var = (struct clientes *)malloc(sizeof(struct clientes) * Quantidade_Clientes);
     FILE *Arquivo_Clientes = fopen("Clientes.txt", "wr");
-    fclose(Arquivo_Clientes);
 
-    for (cont=0; cont<Quantidade_Clientes;cont++)
+    for (contador = 0; contador < Quantidade_Clientes; contador++)
     {
+        fprintf(Arquivo_Clientes, "Dados dos clientes:\n");
+
         printf("Digite o nome do cliente:\n");
-    scanf("%s",Clientes_Var->Nome);
-    printf("Digite o endereco do cliente:\n");
-    scanf("%s", Clientes_Var->Endereco);
-    printf("Digite o codigo do cliente:\n");
-    printf("%s", Clientes_Var->Codigo_de_Cliente);
+        scanf(" %[^\n]", Clientes_Var[contador].Nome);
+        fprintf(Arquivo_Clientes, "Nome:  %s\n", Clientes_Var[contador].Nome);
+
+        printf("Digite o endereco do cliente:\n");
+        scanf(" %[^\n]", Clientes_Var[contador].Endereco);
+        fprintf(Arquivo_Clientes, "Endereco:  %s\n", Clientes_Var[contador].Endereco);
+
+        printf("Digite o codigo do cliente:\n");
+        scanf("%d", &Clientes_Var[contador].Codigo_de_Cliente);
+        fprintf(Arquivo_Clientes, "Codigo de Cliente:  %s\n", Clientes_Var[contador].Codigo_de_Cliente);
     }
     
+    fclose(Arquivo_Clientes);
     
 }

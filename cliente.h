b257812f0a666149cb1282d11c2dad23f33566e4
leticiavieirag <90807534+LeticiaVieirg@users.noreply.github.com>
2024-02-10 21:38:47 -0300

    Update cliente.h

--- cliente.h
+++ cliente.h
@@ -1,6 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/*Renomeacao do tipo estruturado cliente que armazena vetores para os dados*/
 typedef struct clientes Clientes;
 
-void Escrever_dados();
+/*Funcao que preenche os dados dos clientes e os armazena em um arquivo e no vetor*/
+void escrever_dados();
 
 void insertionSort(Clientes *Clientes_Var, int Quantidade_Clientes);

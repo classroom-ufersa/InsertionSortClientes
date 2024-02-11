d78d815c7f07223a2461cddcb3be74ccc39e0b1b
DanielNeres <danielnerres@gmail.com>
2024-02-11 11:21:03 -0300

    Modificação em main.c

--- main.c
+++ main.c
@@ -1,16 +1,10 @@
-#include "cliente.c"
+#include "cliente.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-int contarClientes(FILE *arquivo) {
-  int contador = 0;
-  char linha[100]; // Supõe que uma linha terá no máximo 100 caracteres
 
-  while (fgets(linha, sizeof(linha), arquivo) != NULL) {
-    if (linha[0] != '\n') // Ignora linhas em branco
-      contador++;
-  }
+int main(){
 
-  rewind(arquivo); // Volta ao início do arquivo
-  return contador;
+  Escrever_dados();
+  return 0;
 }

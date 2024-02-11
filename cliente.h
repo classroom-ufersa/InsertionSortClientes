260e2b5af6c11d1d5df26459c3eea44adc1826ee
DanielNeres <danielnerres@gmail.com>
2024-02-11 11:20:34 -0300

    Modificação em cliente.h

--- cliente.h
+++ cliente.h
@@ -1,13 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #ifndef CLIENTES_H
 #define CLIENTES_H
 
 typedef struct clientes Clientes;
 
 /*Funcao que preenche os dados dos clientes e os armazena em um arquivo e no vetor*/
-void escrever_dados();
+void Escrever_dados();
 
 void insertionSort(Clientes **Clientes_Var, int Quantidade_Clientes);
 

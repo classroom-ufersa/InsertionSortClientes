0382d5a6ea0b1eb0c1bc3da0bbe9c9b3d7165acd
Porfirio-Neto <147106025+Porfirio24@users.noreply.github.com>
2024-02-12 10:15:18 -0300

    Update cliente.c

--- cliente.c
+++ cliente.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Definição da estrutura clientes
 struct clientes
 {
   char Nome[80];
@@ -10,14 +11,17 @@ struct clientes
   int Codigo_de_Cliente;
 };
 
+// Função para escrever os dados dos clientes em um arquivo
 void Escrever_dados()
 {
   int Quantidade_Clientes;
   int contador;
 
+  // Solicita a quantidade de clientes ao usuário
   printf("Digite a Quantidade de Clientes:\t");
   scanf("%d", &Quantidade_Clientes);
 
+  // Aloca memória para armazenar os dados dos clientes
   Clientes *Clientes_Var = (Clientes *)malloc(sizeof(Clientes) * Quantidade_Clientes);
   if (Clientes_Var == NULL)
   {
@@ -25,6 +29,7 @@ void Escrever_dados()
     exit(1);
   }
 
+  // Abre o arquivo para escrita
   FILE *Arquivo_Clientes = fopen("Clientes.txt", "w+");
   if (Arquivo_Clientes == NULL)
   {
@@ -32,6 +37,7 @@ void Escrever_dados()
     exit(1);
   }
 
+  // Loop para coletar os dados dos clientes
   for (contador = 0; contador < Quantidade_Clientes; contador++)
   {
     system("cls");
@@ -45,7 +51,10 @@ void Escrever_dados()
     scanf("%d", &Clientes_Var[contador].Codigo_de_Cliente);
   }
 
+  // Chama a função insertionSort para ordenar os clientes pelo nome
   Clientes_Var = insertionSort(Clientes_Var, Quantidade_Clientes);
+
+  // Escreve os dados dos clientes ordenados no arquivo
   for (contador = 0; contador < Quantidade_Clientes; contador++)
   {
     fprintf(Arquivo_Clientes, "Dados dos clientes %d:\n", contador + 1);
@@ -54,10 +63,13 @@ void Escrever_dados()
     fprintf(Arquivo_Clientes, "Codigo de Cliente:  %d\n\n", Clientes_Var[contador].Codigo_de_Cliente);
   }
 
-  fclose(Arquivo_Clientes); // fechar o arquivo
-  free(Clientes_Var);       // Liberar memória alocada
+  // Fecha o arquivo
+  fclose(Arquivo_Clientes);
+  // Libera a memória alocada
+  free(Clientes_Var);
 }
 
+// Função de ordenação insertionSort
 Clientes *insertionSort(Clientes *Clientes_Var, int Quantidade_Clientes)
 {
   int i, j;

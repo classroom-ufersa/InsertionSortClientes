c4950444dcf084c48caabe71a61dcc874a83d4ef
Porfirio-Neto <147106025+Porfirio24@users.noreply.github.com>
2024-02-13 18:39:34 -0300

    Melhoria na organização e formatação cliente.c

--- cliente.c
+++ cliente.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
-#include "cliente.h"
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 // Definição da estrutura clientes
-struct clientes
-{
+typedef struct {
   char Nome[80];
   char Endereco[80];
   int Codigo_de_Cliente;
-};
+} Clientes;
+
+// Protótipos das funções
+void Escrever_dados();
+void string_maiuscula_minuscula(char *str);
+Clientes *insertionSort(Clientes *Clientes_Var, int Quantidade_Clientes);
+
+int main() {
+  Escrever_dados();
+  return 0;
+}
 
 // Função para escrever os dados dos clientes em um arquivo
-void Escrever_dados()
-{
+void Escrever_dados() {
   int Quantidade_Clientes;
   int contador;
 
@@ -23,24 +31,20 @@ void Escrever_dados()
 
   // Aloca memória para armazenar os dados dos clientes
   Clientes *Clientes_Var = (Clientes *)malloc(sizeof(Clientes) * Quantidade_Clientes);
-  if (Clientes_Var == NULL)
-  {
-    printf("Erro na alocacao!!!");
+  if (Clientes_Var == NULL) {
+    printf("Erro na alocação de memória!!!");
     exit(1);
   }
 
   // Abre o arquivo para escrita
   FILE *Arquivo_Clientes = fopen("Clientes.txt", "w+");
-  if (Arquivo_Clientes == NULL)
-  {
-    printf("Erro no arquivo!!!");
+  if (Arquivo_Clientes == NULL) {
+    printf("Erro na abertura do arquivo!!!");
     exit(1);
   }
 
   // Loop para coletar os dados dos clientes
-  for (contador = 0; contador < Quantidade_Clientes; contador++)
-  {
-    system("cls");
+  for (contador = 0; contador < Quantidade_Clientes; contador++) {
     printf("Digite o nome do cliente:\n");
     scanf(" %[^\n]", Clientes_Var[contador].Nome);
     string_maiuscula_minuscula(Clientes_Var[contador].Nome);
@@ -56,8 +60,7 @@ void Escrever_dados()
   Clientes_Var = insertionSort(Clientes_Var, Quantidade_Clientes);
 
   // Escreve os dados dos clientes ordenados no arquivo
-  for (contador = 0; contador < Quantidade_Clientes; contador++)
-  {
+  for (contador = 0; contador < Quantidade_Clientes; contador++) {
     fprintf(Arquivo_Clientes, "Dados dos clientes %d:\n", contador + 1);
     fprintf(Arquivo_Clientes, "Nome:  %s\n", Clientes_Var[contador].Nome);
     fprintf(Arquivo_Clientes, "Endereco:  %s\n", Clientes_Var[contador].Endereco);
@@ -70,35 +73,29 @@ void Escrever_dados()
   free(Clientes_Var);
 }
 
-void string_maiuscula_minuscula(char *str){
-
+void string_maiuscula_minuscula(char *str) {
   int i;
   str[0] = toupper(str[0]);
-  for (i = 1; str[i] != '\0'; i++)
-  {
-    if(isspace(str[i - 1])){ // Se o caractere passado for um espaço
+  for (i = 1; str[i] != '\0'; i++) {
+    if (isspace(str[i - 1])) { // Se o caractere passado for um espaço
       str[i] = toupper(str[i]); // Transforma a letra em maiúscula
-    }
-    else{
+    } else {
       str[i] = tolower(str[i]); // Transforma a letra em minúscula
     }
   }
 }
 
 // Função de ordenação insertionSort
-Clientes *insertionSort(Clientes *Clientes_Var, int Quantidade_Clientes)
-{
+Clientes *insertionSort(Clientes *Clientes_Var, int Quantidade_Clientes) {
   int i, j;
   Clientes key;
 
-  for (i = 1; i < Quantidade_Clientes; i++)
-  {
+  for (i = 1; i < Quantidade_Clientes; i++) {
     key = Clientes_Var[i];
     j = i - 1;
 
     // Comparação completa dos nomes usando strcmp
-    while (j >= 0 && strcmp(Clientes_Var[j].Nome, key.Nome) > 0)
-    {
+    while (j >= 0 && strcmp(Clientes_Var[j].Nome, key.Nome) > 0) {
       Clientes_Var[j + 1] = Clientes_Var[j];
       j--;
     }

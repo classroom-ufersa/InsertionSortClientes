fcfa23fc2f892d0121c5c80c23a878a62e643880
Isabelly Lima <mslima5643@gmail.com>
2024-02-13 18:04:01 -0300

    adicao de comentario no main

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-#include "cliente.h"
+#include "cliente.c"
 #include <stdio.h>
 #include <stdlib.h>
 
 
 int main(){
 
-  Escrever_dados();
+  Escrever_dados(); //Chama a função Escrever_dados que foi implementada no cliente.c
   return 0;
 }

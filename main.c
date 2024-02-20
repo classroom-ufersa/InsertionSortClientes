a77355eaf981cc493f637ec4857cdeb35bf3e6de
DanielNeres <danielnerres@gmail.com>
2024-02-20 11:09:35 -0300

    Modicação em main.c

--- main.c
+++ main.c
@@ -1,33 +1,38 @@
 #include "cliente.c"
 
-int main(){
-  int permanencia, escolha;
-  Clientes *Clientes_var = (Clientes*) malloc(sizeof(Clientes));
-
-  Clientes_var = ler_arquivo(Clientes_var);
-
-do
+int main()
 {
-  permanencia = 0;
-  printf("Caso deseje cadastrar um novo cliente digite 1\n");
-  scanf("%d", escolha);
-  if(escolha == 1){
-  printf("Digite o nome do cliente:\n");
-  scanf(" %[^\n]", Clientes_Var[contador].Nome);
-  string_maiuscula_minuscula(Clientes_Var[contador].Nome);
+  int permanencia, escolha;
+  int *contador_clientes;
+  Clientes *Clientes_var = (Clientes *)malloc(sizeof(Clientes));
 
-  printf("Digite o endereco do cliente:\n");
-  scanf(" %[^\n]", Clientes_Var[contador].Endereco);
+  ler_arquivo(Clientes_var, escolha);
 
-  printf("Digite o codigo do cliente:\n");
-  scanf("%d", &Clientes_Var[contador].Codigo_de_Cliente);
+  do
+  {
+    permanencia = 0;
+    printf("Caso deseje cadastrar um novo cliente digite 1\n");
+    scanf("%d", escolha);
+    if (escolha == 1)
+    {
+      realloc()
+          printf("Digite o nome do cliente:\n");
+      scanf(" %[^\n]", Clientes_Var[contador].Nome);
+      string_maiuscula_minuscula(Clientes_Var[contador].Nome);
 
-  permanencia = 1
-  }
-} while (permanencia == 1)
+      printf("Digite o endereco do cliente:\n");
+      scanf(" %[^\n]", Clientes_Var[contador].Endereco);
 
-  //Chama a função Escrever_dados que foi implementada no cliente.c
-  Escrever_dados(); 
-  
+      printf("Digite o codigo do cliente:\n");
+      scanf("%d", &Clientes_Var[contador].Codigo_de_Cliente);
+
+      permanencia = 1
+    }
+
+  } while (permanencia == 1)
+
+  // Chama a função Escrever_dados que foi implementada no cliente.c
+  Escrever_dados(Clientes_var);
+
   return 0;
 }

e9e77037e17d51dfdfc17b840c70d99ea3addcfb
DanielNeres <danielnerres@gmail.com>
2024-02-20 10:28:57 -0300

    Modifacação em main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
-#include "cliente.h"
+#include "cliente.c"
 
 int main(){
+  Clientes *Clientes_var = (Clientes*) malloc(sizeof(Clientes));
 
+  Clientes_var = ler_arquivo();
   //Chama a função Escrever_dados que foi implementada no cliente.c
   Escrever_dados(); 
   

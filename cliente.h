0452db755fa9d230511a4a65f209fea8131bbe01
DanielNeres <danielnerres@gmail.com>
2024-02-21 15:32:28 -0300

    modificação em cliente.h

--- cliente.h
+++ cliente.h
@@ -1,20 +1,11 @@
 #ifndef CLIENTES_H
 #define CLIENTES_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
 /*Renomeacao de um tipo estruturado para Clientes*/
 typedef struct clientes Clientes;
 
-void Contador_de_Clientes(int *Quantidade_clientes);
-
-void Ler_Arquivo(Clientes *Clientes_Var, int *Quantidade_clientes);
-
 /*Funcao que preenche os dados dos clientes e os armazena em um arquivo e no vetor*/
-void Escrever_dados(Clientes *Clientes_Var, int *Quantidade_clientes);
+void Escrever_dados();
 
 /*Funcao que converte uma string para o formato de capitalizacao mista, 
   Recebe como parametro o ponteiro do tipo caractere*/
@@ -22,6 +13,6 @@ void string_maiuscula_minuscula(char *str);
 
 /*Implemetacao do algoritmo de ordenacao Insertion Sort, 
   Recebe como parametros o ponteiro de cliente e a quantidade do tipo inteiro*/
-void insertionSort(Clientes *Clientes_Var, int *Quantidade_Clientes);
+Clientes *insertionSort(Clientes *Clientes_Var, int Quantidade_Clientes);
 
 #endif // CLIENTES_H
--- main.c
+++ main.c
@@ -1,43 +1,10 @@
-#include "cliente.c"
+#include <stdio.h>
+#include "cliente.h"
 
-int main()
-{
-  int permanencia, escolha;
-  int *Quantidade_clientes;
-  Clientes *Clientes_var = (Clientes *)malloc(sizeof(Clientes));
+int main(){
 
-  Ler_Arquivo(Clientes_var, Quantidade_clientes);
-
-  // Loop para coletar os dados dos clientes
-  do
-  {
-    permanencia = 0;
-    printf("Caso deseje cadastrar um novo cliente digite 1\n");
-    scanf("%d", escolha);
-    if (escolha == 1)
-    {
-      realloc(Quantidade_clientes, *Quantidade_clientes++);
-      printf("Digite o nome do cliente:\n");
-      scanf(" %[^\n]", Clientes_var[*Quantidade_clientes + 1].Nome);
-      string_maiuscula_minuscula(Clientes_var[*Quantidade_clientes + 1].Nome);
-
-      printf("Digite o endereco do cliente:\n");
-      scanf(" %[^\n]", Clientes_var[*Quantidade_clientes + 1].Endereco);
-
-      printf("Digite o codigo do cliente:\n");
-      scanf("%d", &Clientes_var[*Quantidade_clientes + 1].Codigo_de_Cliente);
-
-      permanencia = 1;
-    }
-
-  } while (permanencia == 1);
-  // Chama a função insertionSort para ordenar os clientes pelo nome
-  insertionSort(Clientes_var, Quantidade_clientes);
-
-  // Chama a função Escrever_dados que foi implementada no cliente.c
-  Escrever_dados(Clientes_var, Quantidade_clientes);
-
-  // Libera a memória alocada
-  free(Clientes_var);
+  //Chama a função Escrever_dados que foi implementada no cliente.c
+  Escrever_dados(); 
+  
   return 0;
 }

129185a26680ab33519b46b956c832c29473dbf2
Porfirio-Neto <147106025+Porfirio24@users.noreply.github.com>
2024-02-21 12:26:13 -0300

    Modificação em cliente.h

--- cliente.h
+++ cliente.h
@@ -9,6 +9,8 @@
 /*Renomeacao de um tipo estruturado para Clientes*/
 typedef struct clientes Clientes;
 
+void Contador_de_Clientes(int *Quantidade_clientes);
+
 void Ler_Arquivo(Clientes *Clientes_Var, int *Quantidade_clientes);
 
 /*Funcao que preenche os dados dos clientes e os armazena em um arquivo e no vetor*/
@@ -20,6 +22,6 @@ void string_maiuscula_minuscula(char *str);
 
 /*Implemetacao do algoritmo de ordenacao Insertion Sort, 
   Recebe como parametros o ponteiro de cliente e a quantidade do tipo inteiro*/
-Clientes *insertionSort(Clientes *Clientes_Var, int Quantidade_Clientes);
+void insertionSort(Clientes *Clientes_Var, int *Quantidade_Clientes);
 
 #endif // CLIENTES_H

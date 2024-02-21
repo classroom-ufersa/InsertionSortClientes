c1a7a7ba72d22270676c2eccc2f285ce2ee9fa66
Porfirio-Neto <147106025+Porfirio24@users.noreply.github.com>
2024-02-21 12:27:14 -0300

    Modificação em main.c

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include "cliente.c"
 
-int main(){
+int main()
+{
   int permanencia, escolha;
   int *Quantidade_clientes;
   Clientes *Clientes_var = (Clientes *)malloc(sizeof(Clientes));
 
   Ler_Arquivo(Clientes_var, Quantidade_clientes);
 
-  do {
+  // Loop para coletar os dados dos clientes
+  do
+  {
     permanencia = 0;
     printf("Caso deseje cadastrar um novo cliente digite 1\n");
     scanf("%d", escolha);
-    if (escolha == 1) {
-      realloc(Quantidade_clientes, Quantidade_clientes + 1);
+    if (escolha == 1)
+    {
+      realloc(Quantidade_clientes, *Quantidade_clientes++);
       printf("Digite o nome do cliente:\n");
       scanf(" %[^\n]", Clientes_var[*Quantidade_clientes + 1].Nome);
       string_maiuscula_minuscula(Clientes_var[*Quantidade_clientes + 1].Nome);
@@ -27,9 +31,13 @@ int main(){
     }
 
   } while (permanencia == 1);
+  // Chama a função insertionSort para ordenar os clientes pelo nome
+  insertionSort(Clientes_var, Quantidade_clientes);
 
   // Chama a função Escrever_dados que foi implementada no cliente.c
   Escrever_dados(Clientes_var, Quantidade_clientes);
 
+  // Libera a memória alocada
+  free(Clientes_var);
   return 0;
 }

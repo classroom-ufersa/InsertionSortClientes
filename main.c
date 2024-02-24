63202a0150e0ccef2cfc36765ffb91d09334d361
DanielNeres <danielnerres@gmail.com>
2024-02-24 16:05:33 -0300

    Moficação em main.c

--- main.c
+++ main.c
@@ -1,23 +1,19 @@
 #include "cliente.h"
 
-int main() {
+int main()
+{
   // Aloca memória para armazenar os dados dos clientes
   int permanencia;
   char Escolha;
   int Quantidade_Clientes = Contador_Clientes();
   Clientes *Clientes_Var = (Clientes *)malloc(sizeof(Clientes) * Quantidade_Clientes);
-  if (Clientes_Var == NULL) {
+  if (Clientes_Var == NULL)
+  {
     printf("Erro na alocação de memória!!!");
     exit(1);
   }
 
   Ler_Dados(Clientes_Var, Quantidade_Clientes);
-
-  int contador;
-  for (contador = 0; contador < Quantidade_Clientes; contador++)
-  {
-    printf("nome do cliente %d e == %s\n", contador, Clientes_Var[contador].Nome);
-  }
 
   do
   {
@@ -25,8 +21,9 @@ int main() {
     printf("Caso desege cadastrar algun cliente digite \"S\"\n");
     scanf(" %c", &Escolha);
     Escolha = toupper(Escolha);
-    if (Escolha == 'S'){
-      Clientes_Var = realloc(Clientes_Var, (Quantidade_Clientes + 1) * sizeof(Clientes_Var));
+    if (Escolha == 'S')
+    {
+      Clientes_Var = (Clientes *)realloc(Clientes_Var, (Quantidade_Clientes + 1) * sizeof(Clientes));
       printf("Digite o nome do cliente\n");
       scanf(" %[^\n]", Clientes_Var[Quantidade_Clientes].Nome);
       printf("Digite o endereco do cliente\n");
@@ -37,8 +34,14 @@ int main() {
       Quantidade_Clientes++;
       permanencia = 1;
     }
-  }while(permanencia == 1);
+  } while (permanencia == 1);
 
+  for (; 0 < Quantidade_Clientes; Quantidade_Clientes--)
+  {
+    string_maiuscula_minuscula(Clientes_Var[Quantidade_Clientes - 1].Nome);
+  }
+
+  Clientes_Var = insertionSort(Clientes_Var, Quantidade_Clientes);
   // Chama a função Escrever_dados que foi implementada no cliente.c
   Escrever_dados(Clientes_Var, Quantidade_Clientes);
 

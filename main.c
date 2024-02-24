df3b9391eb60f7a8676a54f319ac4d13daa87f1b
DanielNeres <danielnerres@gmail.com>
2024-02-24 14:11:08 -0300

    Modificação em main.c

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 int main() {
   // Aloca memória para armazenar os dados dos clientes
+  int permanencia;
+  char Escolha;
   int Quantidade_Clientes = Contador_Clientes();
   Clientes *Clientes_Var = (Clientes *)malloc(sizeof(Clientes) * Quantidade_Clientes);
   if (Clientes_Var == NULL) {
@@ -10,7 +12,26 @@ int main() {
   }
 
   Ler_Dados(Clientes_Var, Quantidade_Clientes);
-  printf("dentro do ler_dados == %d\n", Quantidade_Clientes);
+
+  do
+  {
+    permanencia = 0;
+    printf("Caso desege cadastrar algun cliente digite \"S\"\n");
+    scanf("%c", &Escolha);
+    Escolha = toupper(Escolha);
+    if (Escolha == 'S'){
+      printf("Digite o nome do cliente\n");
+      scanf("%[^\n]", Clientes_Var[Quantidade_Clientes].Nome);
+      printf("Digite o endereco do cliente\n");
+      scanf("%[^\n]", Clientes_Var[Quantidade_Clientes].Nome);
+      printf("Digite o codigo do cliente\n");
+      scanf("%d", &Clientes_Var[Quantidade_Clientes].Nome);
+      Quantidade_Clientes++;
+      permanencia = 1;
+    }
+  }while(permanencia = 1);
+
+  Clientes_Var = insertionSort(Clientes_Var, Quantidade_Clientes);
 
   // Chama a função Escrever_dados que foi implementada no cliente.c
   Escrever_dados(Clientes_Var, Quantidade_Clientes);

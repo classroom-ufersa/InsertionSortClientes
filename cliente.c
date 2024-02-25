32b69d457375630f44115f168763cf47af5bb894
Isabelly Lima <mslima5643@gmail.com>
2024-02-25 09:48:21 -0300

    update cliente.c

--- cliente.c
+++ cliente.c
@@ -20,26 +20,26 @@ int Contador_Clientes(){ //funcao para contar o número de vezes que a string "D
 
 void Ler_Dados(Clientes *Clientes_Var, int Quantidade_Clientes){ //criacao da função ler dados dos clientes
   int contador; //criacao de um contador, para determinar quantas vezes o loop deve acontecer
-  FILE *Arquivo_Clientes = fopen("Clientes.txt", "rt");
-  if (Arquivo_Clientes == NULL) {
+  FILE *Arquivo_Clientes = fopen("Clientes.txt", "rt"); //abre o arquivo para para em modo leitura
+  if (Arquivo_Clientes == NULL) { //verifica se o arquivo foi aberto corretamente, se nao, aparece uma mensagem de erro na tela e o programa eh encerrado
     printf("Erro na abertura do arquivo\n");
     exit(1);
   }
-  for (contador = 0; contador < Quantidade_Clientes; contador++) {
-    fscanf(Arquivo_Clientes, "Dados dos clientes:\nNome: %[^\n]\nEndereco: %[^\n]\nCodigo de Cliente: %d\n\n", Clientes_Var[contador].Nome, Clientes_Var[contador].Endereco, &Clientes_Var[contador].Codigo_de_Cliente);
+  for (contador = 0; contador < Quantidade_Clientes; contador++) { //Enquanto o loop acontecer a funcao fscanf vai ser usada
+    fscanf(Arquivo_Clientes, "Dados dos clientes:\nNome: %[^\n]\nEndereco: %[^\n]\nCodigo de Cliente: %d\n\n", Clientes_Var[contador].Nome, Clientes_Var[contador].Endereco, &Clientes_Var[contador].Codigo_de_Cliente); //Usa fscanf para ler os dados do cliente do arquivo.
   }
-  fclose(Arquivo_Clientes);
+  fclose(Arquivo_Clientes); //fecha o arquivo
 }
 
 void Escrever_dados(Clientes *Clientes_Var, int Quantidade_Clientes){ //criação de uma função para colocar os dados dos clientes no arquivo txt
   int contador; //criancao de uma variavel inteira 
-  FILE *Arquivo_Clientes = fopen("Clientes.txt", "wt");
-  if (Arquivo_Clientes == NULL)  {
+  FILE *Arquivo_Clientes = fopen("Clientes.txt", "wt"); //abertura de um arquivo em modo de escrita
+  if (Arquivo_Clientes == NULL)  { //Verifica se o arquivo foi aberto corretamente, se nao, aparece uma mensagem de erro na tela e o programa eh encerrado
     printf("Erro na abertura do arquivo\n");
     exit(1);
   }
-  for (contador = 0; contador < Quantidade_Clientes; contador++)  { //criacao de um loop que vai cadatrar os dados dos clientes no arquivo de texto, até Quantidade_Clientes-1
-    fprintf(Arquivo_Clientes, "Dados dos clientes:\n");
+  for (contador = 0; contador < Quantidade_Clientes; contador++)  { //criacao de um loop que vai cadatrar os dados dos clientes no arquivo de texto, até o conatador for Quantidade_Clientes-1
+    fprintf(Arquivo_Clientes, "Dados dos clientes:\n")
     fprintf(Arquivo_Clientes, "Nome:  %s\n", Clientes_Var[contador].Nome);
     fprintf(Arquivo_Clientes, "Endereco:  %s\n", Clientes_Var[contador].Endereco);
     fprintf(Arquivo_Clientes, "Codigo de Cliente:  %d\n\n", Clientes_Var[contador].Codigo_de_Cliente);

f1630b0b1d0dfaf937181c2640ae331db0beba4a
Isabelly Lima <mslima5643@gmail.com>
2024-02-25 09:38:25 -0300

    update cliente.c

--- cliente.c
+++ cliente.c
@@ -1,24 +1,25 @@
 #include "cliente.h"
 
-int Contador_Clientes(){
-  char linha[200];
-  int Quantidade_Clientes = 0;
-  FILE *Arquivo_Clientes = fopen("Clientes.txt", "rt");
-  if (Arquivo_Clientes == NULL) {
+int Contador_Clientes(){ //funcao para contar o número de vezes que a string "Dados dos clientes:
+  char linha[200];  // essa variavel é usada para armazenar temporariamente cada linha lida do arquivo "Clientes.txt"
+  int Quantidade_Clientes = 0; //inicializa a variavel com 0
+  FILE *Arquivo_Clientes = fopen("Clientes.txt", "rt"); //abre um aqruivo txt para leitura
+  if (Arquivo_Clientes == NULL) { //verifica se o arquivo foi aberto corretamente, se nao, aparece uma mensagem de erro na tela e o programa eh encerrado
     printf("Erro na abertura do arquivo\n");
     exit(1);
   }
-  while (fgets(linha, 200, Arquivo_Clientes) != NULL) {
-    if (strstr(linha, "Dados dos clientes:")) {
+  while (fgets(linha, 200, Arquivo_Clientes) != NULL) { //esse loop eh feito para ler uma linha do arquivo e armazená-la na variavel linha.
+    if (strstr(linha, "Dados dos clientes:")) { //lenr o arquivo linha por linha e usando a função strstr para buscar a string dentro de cada linha
+        // se apareceu a string "Dados dos clientes" se sim, a cada vez que aparecer,o contador Quantidade_Clientes eh incrementado
       Quantidade_Clientes++;
     }
   }
-  fclose(Arquivo_Clientes);
-  return Quantidade_Clientes;
+  fclose(Arquivo_Clientes); //fecha o arquivo
+  return Quantidade_Clientes; //retorna quantas vezes "Dados de clientes" apareceu
 }
 
-void Ler_Dados(Clientes *Clientes_Var, int Quantidade_Clientes){
-  int contador;
+void Ler_Dados(Clientes *Clientes_Var, int Quantidade_Clientes){ //criacao da função ler dados dos clientes
+  int contador; //criacao de um contador, para determinar quantas vezes o loop deve acontecer
   FILE *Arquivo_Clientes = fopen("Clientes.txt", "rt");
   if (Arquivo_Clientes == NULL) {
     printf("Erro na abertura do arquivo\n");
@@ -30,23 +31,23 @@ void Ler_Dados(Clientes *Clientes_Var, int Quantidade_Clientes){
   fclose(Arquivo_Clientes);
 }
 
-void Escrever_dados(Clientes *Clientes_Var, int Quantidade_Clientes){
-  int contador;
+void Escrever_dados(Clientes *Clientes_Var, int Quantidade_Clientes){ //criação de uma função para colocar os dados dos clientes no arquivo txt
+  int contador; //criancao de uma variavel inteira 
   FILE *Arquivo_Clientes = fopen("Clientes.txt", "wt");
   if (Arquivo_Clientes == NULL)  {
     printf("Erro na abertura do arquivo\n");
     exit(1);
   }
-  for (contador = 0; contador < Quantidade_Clientes; contador++)  {
+  for (contador = 0; contador < Quantidade_Clientes; contador++)  { //criacao de um loop que vai cadatrar os dados dos clientes no arquivo de texto, até Quantidade_Clientes-1
     fprintf(Arquivo_Clientes, "Dados dos clientes:\n");
     fprintf(Arquivo_Clientes, "Nome:  %s\n", Clientes_Var[contador].Nome);
     fprintf(Arquivo_Clientes, "Endereco:  %s\n", Clientes_Var[contador].Endereco);
     fprintf(Arquivo_Clientes, "Codigo de Cliente:  %d\n\n", Clientes_Var[contador].Codigo_de_Cliente);
   }
-  fclose(Arquivo_Clientes);
+  fclose(Arquivo_Clientes); //fecha o arquivo de texto
 }
 
-void string_maiuscula_minuscula(char *Nome){
+void string_maiuscula_minuscula(char *Nome){ //criacao de uma funcao para mudar a primeira letra do 
   int Contador;
   Nome[0] = toupper(Nome[0]);
   for (Contador = 1; Nome[Contador] != '\0'; Contador++)  {

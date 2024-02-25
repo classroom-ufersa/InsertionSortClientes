3b7160590fc55868773f7cc9ac79e0db7886e8ee
Isabelly Lima <mslima5643@gmail.com>
2024-02-25 10:16:04 -0300

    UPDATE main.c

--- main.c
+++ main.c
@@ -2,20 +2,20 @@
 
 int main() {
 
-  int permanencia, contador; //criacao de duas variaveis inteiras, um vai servir de contador em um loop e a outra vai controlar outro loop
-  char Escolha; //criacao de uma variavel char, que vai ler um caractere
-  int Quantidade_Clientes = Contador_Clientes(); // Chama a função Contador_Clientes para obter a quantidade inicial de clientes
+  int permanencia, contador; //Criacao de duas variaveis inteiras, um vai servir de contador em um loop e a outra vai controlar outro loop.
+  char Escolha; //Criacao de uma variavel char.
+  int Quantidade_Clientes = Contador_Clientes(); //Chama a função Contador_Clientes para obter a quantidade inicial de clientes.
  
-  Clientes *Clientes_Var = (Clientes *)malloc(sizeof(Clientes) * Quantidade_Clientes);   // Aloca memória para armazenar os dados dos clientes
-  if (Clientes_Var == NULL) { //verifica se a alocacao foi bem sucedida, se nao, aparece uma mensagem de erro na tela e o programa eh encerrado
+  Clientes *Clientes_Var = (Clientes *)malloc(sizeof(Clientes) * Quantidade_Clientes);   // Aloca memória para armazenar os dados dos clientes.
+  if (Clientes_Var == NULL) { //verifica se a alocacao foi bem sucedida, se nao, aparece uma mensagem de erro na tela e o programa eh encerrado.
     printf("Erro na alocação de memória!!!");
     exit(1);
   }
 
-  Ler_Dados(Clientes_Var, Quantidade_Clientes); //chama a funcao Ler_Dados
+  Ler_Dados(Clientes_Var, Quantidade_Clientes); //Chama a funcao Ler_Dados
 
   do  {
-    permanencia = 0; // Torna falso a condição do loop, ou seja, caso não entre no if o loop se encerra
+    permanencia = 0; // Torna falso a condição do loop, ou seja, caso não entre no if o loop se encerra.
     printf("Caso desege cadastrar algun cliente digite \"S\"\n");
     scanf(" %c", &Escolha); // Recebe a letra escolhida pelo o usuario
     Escolha = toupper(Escolha); // Torna ela sempre maiuscula
@@ -26,27 +26,27 @@ int main() {
 
       system("cls"); // Limpa tela
       printf("Digite o nome do cliente\n");
-      scanf(" %[^\n]", Clientes_Var[Quantidade_Clientes].Nome); // Recebe o nome do novo cliente
+      scanf(" %[^\n]", Clientes_Var[Quantidade_Clientes].Nome); // Recebe o nome do novo cliente.
       printf("Digite o endereco do cliente\n");
-      scanf(" %[^\n]", Clientes_Var[Quantidade_Clientes].Endereco); // Recebe o endereço do novo cliente
+      scanf(" %[^\n]", Clientes_Var[Quantidade_Clientes].Endereco); // Recebe o endereço do novo cliente.
       printf("Digite o codigo do cliente\n");
-      scanf("%d", &Clientes_Var[Quantidade_Clientes].Codigo_de_Cliente); // Recebe o enderço do novo cliente
+      scanf("%d", &Clientes_Var[Quantidade_Clientes].Codigo_de_Cliente); // Recebe o enderço do novo cliente.
 
-      Quantidade_Clientes++; // Incrementa +1, de registrar o novo cliente cadastrado
-      permanencia = 1; // Torna verdadeira a condição do loop
+      Quantidade_Clientes++; //Incrementa +1, de registrar o novo cliente cadastrado.
+      permanencia = 1; //Torna verdadeira a condição do loop.
     }
-  } while (permanencia == 1); //encerra o loop
+  } while (permanencia == 1); //Encerra o loop.
 
-  for (contador = 0; contador < Quantidade_Clientes; contador++)  { //Entra no loop que vai percorrer todos os clientes, tornando maiuscula a primeira letra do nome
+  for (contador = 0; contador < Quantidade_Clientes; contador++)  { //Entra no loop que vai percorrer todos os clientes, tornando maiuscula a primeira letra do nome.
     string_maiuscula_minuscula(Clientes_Var[contador].Nome);
   }
 
-  Clientes_Var = insertionSort(Clientes_Var, Quantidade_Clientes);
+  Clientes_Var = insertionSort(Clientes_Var, Quantidade_Clientes); //chama a função insertionSort para ordenar o array de estruturas.
 
-  // Chama a função Escrever_dados que foi implementada no cliente.c
+  //Chama a função Escrever_dados que foi implementada no cliente.c.
   Escrever_dados(Clientes_Var, Quantidade_Clientes);
 
-  // Libera a memória alocada
+  //Libera a memória alocada
   free(Clientes_Var);
 
   return 0;
